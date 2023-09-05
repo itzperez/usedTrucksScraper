0c3b5d2c8102dd64e5e75e60424b48dbc35ec289
itzperez <76832936+itzperez@users.noreply.github.com>
2023-09-05 13:26:15 -0700

    Initial commit

---
+++ createDataSet.py
@@ -0,0 +1,59 @@
+import csv
+from scraper import scraper
+
+""" 
+-> I used Zenrows free trial to scrape these urls 
+-> I call the Zenrows api in 'scraper.py' lines 5, 6, 13
+-> Free trial only fetches 20 urls
+-> I had to create three differnt free trial accounts in order to scrape these urls
+-> With the first trial, I scraped the first three urls ('BioDiesel' & 'Diesel' commented out)
+-> With the second trial, I scraped the fourth url ('Electric', 'Flex Fuel', 'Natural Gas', & 'Diesel' commented out )
+-> With the third trial, I scraped the fifth url ('Electric', 'Flex Fuel', 'Natural Gas', & 'BioDiesel' commented out )
+-> When switching trials, the Zenrows API key needs to be updated on 'scraper.py' line 5 """
+urls = [
+    # {'fuelType': 'Electric', 'url': 'https://www.commercialtrucktrader.com/Used-electric/trucks-for-sale?condition=U&fuelType=electric&class=CLASS%204%20%28GVW%2014001%20-%2016000%29%7C4%2CCLASS%205%20%28GVW%2016001%20-%2019500%29%7C5%2CCLASS%206%20%28GVW%2019501%20-%2026000%29%7C6%2CCLASS%207%20%28GVW%2026001%20-%2033000%29%7C7%2CCLASS%208%20%28GVW%2033001%20-%20150000%29%20%7C8&page=1'},
+    # {'fuelType': 'Flex Fuel', 'url': 'https://www.commercialtrucktrader.com/flexFuel/trucks-for-sale?fuelType=flexFuel&lat=37.75&long=-122.44&sort=create_date%3Adesc&class=CLASS%204%20(GVW%2014001%20-%2016000)%7C4%2CCLASS%205%20(GVW%2016001%20-%2019500)%7C5%2CCLASS%206%20(GVW%2019501%20-%2026000)%7C6&page=1'},
+    # {'fuelType': 'Natural Gas', 'url': 'https://www.commercialtrucktrader.com/Used-naturalGas/trucks-for-sale?condition=U&fuelType=naturalGas&sort=create_date%3Adesc&class=CLASS%205%20%28GVW%2016001%20-%2019500%29%7C5%2CCLASS%204%20%28GVW%2014001%20-%2016000%29%7C4%2CCLASS%206%20%28GVW%2019501%20-%2026000%29%7C6%2CCLASS%207%20%28GVW%2026001%20-%2033000%29%7C7%2CCLASS%208%20%28GVW%2033001%20-%20150000%29%20%7C8&page=1'},
+    # {'fuelType': 'BioDiesel', 'url': 'https://www.commercialtrucktrader.com/Used-bioDiesel/trucks-for-sale?condition=U&fuelType=bioDiesel&sort=create_date%3Adesc&class=CLASS%205%20%28GVW%2016001%20-%2019500%29%7C5%2CCLASS%204%20%28GVW%2014001%20-%2016000%29%7C4%2CCLASS%206%20%28GVW%2019501%20-%2026000%29%7C6%2CCLASS%207%20%28GVW%2026001%20-%2033000%29%7C7%2CCLASS%208%20%28GVW%2033001%20-%20150000%29%20%7C8&page=1'},
+    {'fuelType': 'Diesel', 'url': 'https://www.commercialtrucktrader.com/Used-diesel/trucks-for-sale?condition=U&fuelType=diesel&sort=create_date%3Adesc&class=CLASS%205%20%28GVW%2016001%20-%2019500%29%7C5%2CCLASS%204%20%28GVW%2014001%20-%2016000%29%7C4%2CCLASS%206%20%28GVW%2019501%20-%2026000%29%7C6%2CCLASS%207%20%28GVW%2026001%20-%2033000%29%7C7%2CCLASS%208%20%28GVW%2033001%20-%20150000%29%20%7C8&page=1'}
+]
+
+rows = []
+for elem in urls:
+    print('FuelType:', elem['fuelType'])
+    result = scraper(elem['fuelType'], elem['url'])
+    rows.extend(result)
+
+#name of csv file
+filename = 'usedTrucks.csv'
+
+#IF CREATING DATA SET FOR THE FIRST TIME, (using the first free trial) use lines 31 - 54 and comment out 56 - 58
+fields = [
+    'id',
+    'price',
+    'class',
+    'make',
+    'model',
+    'year',
+    'fuelType',
+    'mileage',
+    'description',
+    'city',
+    'state',
+    'sellerType',
+    'listingDateMonth',
+    'listingDateYear',
+    'url',
+    'timestamp'
+]
+
+with open(filename, 'w') as file:
+    writerObject = csv.writer(file)
+    writerObject.writerow(fields)
+    writerObject.writerows(rows)
+
+#IF CONTINUING TO WRITE THE DATA SET, (using the second trial & third trial), comment out lines 31 - 54 and uncomment 56 - 58
+# with open(filename, 'a', newline='') as file:
+#     writerObject = csv.writer(file)
+#     writerObject.writerows(rows)
+    
